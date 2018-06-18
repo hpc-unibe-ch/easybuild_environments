8492df58f3d5ac4bafdb213781cee91dfc5c2b15
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2018-06-18 22:20:06 +0200

    Installed tmux-2.7.eb

---
+++ modulefiles/all/libevent/.2.1.8.lua
@@ -0,0 +1,33 @@
+help([==[
+
+Description
+===========
+The libevent API provides a mechanism to execute a callback function when a specific
+ event occurs on a file descriptor or after a timeout has been reached.
+ Furthermore, libevent also support callbacks due to signals or regular timeouts.
+
+
+More information
+================
+ - Homepage: http://libevent.org/
+]==])
+
+whatis([==[Description: The libevent API provides a mechanism to execute a callback function when a specific
+ event occurs on a file descriptor or after a timeout has been reached.
+ Furthermore, libevent also support callbacks due to signals or regular timeouts.]==])
+whatis([==[Homepage: http://libevent.org/]==])
+
+local root = "/software.el7/software/libevent/2.1.8"
+
+conflict("libevent")
+
+prepend_path("CPATH", pathJoin(root, "include"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
+prepend_path("LIBRARY_PATH", pathJoin(root, "lib"))
+prepend_path("PATH", pathJoin(root, "bin"))
+prepend_path("PKG_CONFIG_PATH", pathJoin(root, "lib/pkgconfig"))
+setenv("EBROOTLIBEVENT", root)
+setenv("EBVERSIONLIBEVENT", "2.1.8")
+setenv("EBDEVELLIBEVENT", pathJoin(root, "easybuild/libevent-.2.1.8-easybuild-devel"))
+
+-- Built with EasyBuild version 3.6.1
