04d56f83a03cd898542f3d183801a613f5d40b10
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2019-01-10 15:41:05 +0100

    Rebuilt OpenMPI-2.1.2-iccifort-2018.1.163-GCC-6.4.0-2.28.eb due to Slurm upgrade

--- modulefiles/all/OpenMPI/2.1.2-iccifort-2018.1.163-GCC-6.4.0-2.28.lua
+++ modulefiles/all/OpenMPI/2.1.2-iccifort-2018.1.163-GCC-6.4.0-2.28.lua
@@ -35,4 +35,4 @@ setenv("EBROOTOPENMPI", root)
 setenv("EBVERSIONOPENMPI", "2.1.2")
 setenv("EBDEVELOPENMPI", pathJoin(root, "easybuild/OpenMPI-2.1.2-iccifort-2018.1.163-GCC-6.4.0-2.28-easybuild-devel"))
 
--- Built with EasyBuild version 3.6.1
+-- Built with EasyBuild version 3.7.1
