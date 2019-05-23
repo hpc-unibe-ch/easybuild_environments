1a1372a6519f4e9e2845a2f30f87cf1c8ee10a27
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2019-05-23 15:04:47 +0200

    Installed EasyBuild-3.9.1

---
+++ modulefiles/all/EasyBuild/3.9.1.lua
@@ -0,0 +1,30 @@
+help([==[
+
+Description
+===========
+EasyBuild is a software build and installation framework
+ written in Python that allows you to install software in a structured,
+ repeatable and robust way.
+
+
+More information
+================
+ - Homepage: https://easybuilders.github.io/easybuild
+]==])
+
+whatis([==[Description: EasyBuild is a software build and installation framework
+ written in Python that allows you to install software in a structured,
+ repeatable and robust way.]==])
+whatis([==[Homepage: https://easybuilders.github.io/easybuild]==])
+
+local root = "/software.el7/software/EasyBuild/3.9.1"
+
+conflict("EasyBuild")
+
+prepend_path("PATH", pathJoin(root, "bin"))
+setenv("EBROOTEASYBUILD", root)
+setenv("EBVERSIONEASYBUILD", "3.9.1")
+setenv("EBDEVELEASYBUILD", pathJoin(root, "easybuild/EasyBuild-3.9.1-easybuild-devel"))
+
+prepend_path("PYTHONPATH", pathJoin(root, "lib/python2.7/site-packages"))
+-- Built with EasyBuild version 3.7.1
