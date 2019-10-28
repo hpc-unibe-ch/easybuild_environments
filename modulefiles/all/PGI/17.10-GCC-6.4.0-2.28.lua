a06b7ae5b122742c3b700268dfb773c9d5f58e5a
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2019-10-28 11:15:04 +0100

    Rebuilt PGI-17.10-GCC-6.4.0-2.28.eb

--- modulefiles/all/PGI/17.10-GCC-6.4.0-2.28.lua
+++ modulefiles/all/PGI/17.10-GCC-6.4.0-2.28.lua
@@ -12,20 +12,21 @@ More information
 
 whatis([==[Description: C, C++ and Fortran compilers from The Portland Group - PGI]==])
 whatis([==[Homepage: http://www.pgroup.com/]==])
+whatis([==[URL: http://www.pgroup.com/]==])
 
 local root = "/software.el7/software/PGI/17.10-GCC-6.4.0-2.28"
 
 conflict("PGI")
 
-if not isloaded("GCCcore/.6.4.0") then
+if not ( isloaded("GCCcore/.6.4.0") ) then
     load("GCCcore/.6.4.0")
 end
 
-if not isloaded("binutils/.2.28-GCCcore-6.4.0") then
+if not ( isloaded("binutils/.2.28-GCCcore-6.4.0") ) then
     load("binutils/.2.28-GCCcore-6.4.0")
 end
 
-if not isloaded("numactl/2.0.11-GCCcore-6.4.0") then
+if not ( isloaded("numactl/2.0.11-GCCcore-6.4.0") ) then
     load("numactl/2.0.11-GCCcore-6.4.0")
 end
 
@@ -40,4 +41,4 @@ setenv("EBDEVELPGI", pathJoin(root, "easybuild/PGI-17.10-GCC-6.4.0-2.28-easybuil
 prepend_path("PATH", root)
 prepend_path("PGROUPD_LICENSE_FILE", "7496@lizenzserver-pgi.ubelix.unibe.ch")
 setenv("PGI", "/software.el7/software/PGI/17.10-GCC-6.4.0-2.28")
--- Built with EasyBuild version 3.6.1
+-- Built with EasyBuild version 3.9.3
